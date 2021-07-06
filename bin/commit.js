5e64a856ef80ba8b69a765439e3d872f38160410
Steven Lopes <slopes@heartcup.com>
2021-07-06 16:48:59 -0700

    Removing the extra script to isolate husky install

--- bin/commit.js
+++ bin/commit.js
@@ -1,4 +1,4 @@
 #! /usr/bin/env node
 let shell = require("shelljs");
-shell.exec("cd ../../ npm install husky");
+//shell.exec("cd ../../ npm install husky");
 //shell.exec("git add -A . && git commit -a -m 'gh-pages update'");
