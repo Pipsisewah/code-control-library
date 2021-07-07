f8a918b48d53c6948acfe82e5d6ff438549a49a1
Steven Lopes <slopes@heartcup.com>
2021-07-06 17:41:28 -0700

    Modified bin script

--- bin/commit.js
+++ bin/commit.js
@@ -1,6 +1,6 @@
 #! /usr/bin/env node
 let shell = require("shelljs");
 shell.exec("echo 'Running Script'");
-shell.exec("cd ../../ npm run prepare");
+shell.exec("cd ../../ && npm run prepare");
 //shell.exec("cd ../../ npm install husky");
 //shell.exec("git add -A . && git commit -a -m 'gh-pages update'");
