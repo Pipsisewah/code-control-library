1b223fe457c9174647e39f14c398f754012cd188
Steven Lopes <slopes@heartcup.com>
2021-07-06 21:25:55 -0700

    Cleaned up installation and removed most unused items

---
+++ README.md
@@ -0,0 +1,2 @@
+## How to add a command
+npx husky add .husky/pre-commit 'npm test'
--- scripts/install.js
+++
@@ -1,3 +0,0 @@
-#! /usr/bin/env node
-const { spawn } = require('child_process');
-spawn("cd ../../ && npx husky-init && npx husky add .husky/pre-commit 'npm test'");
